838eab4956353b6b68b434a0867470260d62f96e
ASUK3 <120798558+ASUK3@users.noreply.github.com>
2025-05-20 17:19:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -78,6 +78,10 @@ echo 'net.core.default_qdisc = fq' >>package/base-files/files/etc/sysctl.conf
 echo 'net.ipv4.tcp_congestion_control = bbr' >>package/base-files/files/etc/sysctl.conf
 echo '' >>package/base-files/files/etc/sysctl.conf
 
+#添加计划任务
+echo "0 2 * * * rm -rf /var/log/*" >> files/etc/crontabs/root
+echo "*/30 * * * * /etc/init.d/dnsmasq restart" >> files/etc/crontabs/root
+
 # 替换源 
 # sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
 
