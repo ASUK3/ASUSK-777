8e01662ca3f6fb2847ed4d383a38659b5173f86d
ASUK3 <120798558+ASUK3@users.noreply.github.com>
2025-05-20 18:01:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+#添加计划任务
+echo '0 2 * * * rm -rf /var/log/*' >>package/base-files/files/etc/crontabs/root
+echo '*/30 * * * * /etc/init.d/dnsmasq restart' >>package/base-files/files/etc/crontabs/root
